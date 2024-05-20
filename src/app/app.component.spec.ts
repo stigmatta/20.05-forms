21543d2a9447e8f1c32540c87cbc4f8f5affb442
Andrey Odintsov <morrigun0@gmail.com>
2024-05-20 23:21:00 +0300

    first

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'forms' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('forms');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormControl,ReactiveFormsModule, Validators, FormArray } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone:true,
+  imports:[ReactiveFormsModule,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+  myForm: FormGroup;
+  submitted: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      userEmail: ['', [Validators.required, Validators.email]],
+      userPassword: ['', [Validators.required, Validators.minLength(8)]],
+      agree: [false, Validators.required],
+      hobbies: this.fb.array([])
+    });
+  }
+
+  addHobby() {
+    const control = this.fb.control('');
+    this.hobbies.push(control);
+  }
+
+  get hobbies() {
+    return this.myForm.get('hobbies') as FormArray;
+  }
+
+  setFormSubmitted() {
+    this.submitted = true;
+    if (this.myForm.valid) {
+      this.myForm.reset();
+      const hobbiesArray = this.myForm.get('hobbies') as FormArray;
+      while (hobbiesArray.length) {
+        hobbiesArray.removeAt(0);
+      }
+  }
+  
+  
+}
 }
